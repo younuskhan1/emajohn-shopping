412124b795f72b268262bfd9afb8265d3c0b3070
younuskhan1 <md.younuskhan412@gmail.com>
2023-12-20 22:18:40 +0600

    card-button is added now

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,12 +1,19 @@
 import './Card.css';
 const Card = ({product}) => {
-    const {img, name, price} = product;
+    console.log(product);
+    const {img, name, price, seller, ratings} = product;
     return (
-        <div className="card-container">
-            <img className="card-image" src={img} alt="" />
-            <h3>{name}</h3>
-            <p>{price}</p>
-           
+        <div className='card-container' >
+            <div className="card-img-text-container">
+                <img className="card-image" src={img} alt="" />
+                <div style={{marginTop:"20px", lineHeight: "1.8em"}}>
+                    <h3 style={{textAlign:"left"}}>{name}</h3>
+                    <p style={{textAlign:"left",fontSize:"18px", fontWeight:"bolder"}}> Price : {price}</p>
+                    <p style={{textAlign:"left",fontSize:"18px", fontWeight:"bolder"}}> Manufacturer : {seller}</p>
+                    <p style={{textAlign:"left",fontSize:"18px", fontWeight:"bolder"}}> Ratings : {ratings} stars </p>
+                </div>   
+            </div>
+            <p className='card-button'>Add To Cart</p>
         </div>
     );
 };
